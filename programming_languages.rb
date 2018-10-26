76b21e19ec50ea0dd8c78abd14d26df8cdc4ad48
Joe Quattrone <joequattrone5@gmail.com>
2018-10-26 19:42:19 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -43,6 +43,7 @@ def reformat_languages(languages)
         new_hash[language] ||= {}
         new_hash[language][type_key] = type_value
         new_hash[language][:style] = style
+      end
     end
   end
   new_hash
